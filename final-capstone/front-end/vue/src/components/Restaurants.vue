c966fcb86ee9864c67e216724b1048d3816699d1
Brianna Williams <briwill146@gmail.com>
2020-12-08 17:01:37 -0500

    restaurant styling, blanchedalmond

--- final-capstone/front-end/vue/src/components/Restaurants.vue
+++ final-capstone/front-end/vue/src/components/Restaurants.vue
@@ -6,7 +6,8 @@
             <div class="restaurant-head"
                 v-for="restaurant in restaurants"
                 v-bind:key="restaurant.id">
-                <h4>{{ restaurant.restaurantName }}</h4>
+                <h1>{{ restaurant.restaurantName }}</h1>
+                <h4>restaurant type</h4>
             <!-- </div>
 
             <div class="rest-image-link">     -->
@@ -26,6 +27,12 @@ export default {
         return {
             restaurants: []
         }
+        /* ,
+        getTypeName() {
+            if(restaurant.typeId === restaurantType.typeId) {
+                return restaurantType.typeName;
+            }
+        }, */
     },
 created() {
     theApplicationService.getAllRestaurants()
@@ -41,7 +48,7 @@ created() {
 
 .restaurant-page {
     background-image: linear-gradient(to bottom left,  #FF655B, #FD297B);;
-
+    height: 45rem
 }
 .restaurant-items {
     display: flex;
@@ -52,13 +59,19 @@ created() {
 
 .restaurant-head {
     text-align: center;
-}
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: em;
+    text-shadow: 2px 5px 5px rgba(8, 8, 8, 0.75),
+                -5px 6px 7px rgba(8, 8, 8, 0.75);
+    color: blanchedalmond;
+    }
 .rest-images {
     height: 250px;
     width: 250px;
-    /* border-style: dotted;
-    border: black;
-    border-radius: 5px;  */
+    border-style: solid;
+    border-color: black;
+    border-width: 2px;
+    border-radius: 5px; 
     } 
 
 </style>
--- final-capstone/front-end/vue/src/components/StartupPage.vue
+++ final-capstone/front-end/vue/src/components/StartupPage.vue
@@ -58,7 +58,7 @@ export default {
     flex-direction: column;
     justify-content: center; /*centers the items within this box*/
     align-items: center;
-    background-color: white;
+    background-color: blanchedalmond;
     height: 250px;
     width: 250px;
     border-radius: 5%;
