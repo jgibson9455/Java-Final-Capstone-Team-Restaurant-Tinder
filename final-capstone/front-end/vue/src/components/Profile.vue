921e1e9226c3a81a0106610dd3cff9271ba6d72f
Amanda Wolf <amandamwolf24@gmail.com>
2020-12-08 18:51:30 -0500

    my profile

--- final-capstone/front-end/vue/src/components/Profile.vue
+++ final-capstone/front-end/vue/src/components/Profile.vue
@@ -1,10 +1,13 @@
 <template>
 <body class="profile">
+  <div class="space"></div>
+
+  <h1 id="my-profile"> My Profile</h1>
+
    <div class="home">
     
-    <h1> My Profile</h1>
     <div class="welcome">
-    <p>Here is where you can view a list of restaurants as well as look at your favorited restaurants that you saved for later. <br>
+    <p>Here is where you can view a list of restaurants as well as look at your favorited restaurants that you saved for later.
     Also, you can update your profile as well as choose your food preferences with the Food Questionnaire.</p>
     </div>
 
@@ -13,10 +16,25 @@
    <router-link class="profile-navs" v-bind:to="{name: 'questionnaire'}">Food Questionnaire</router-link>
     </div>
 
-    
-
    <router-link class="restaurant-navs" v-bind:to="{name: 'restaurants'}">View Restaurant List</router-link> 
+
+   <p class="start">Start</p>
    
+   <h3 class="fav">Favorite Restaurants</h3>
+   <div class="fav-temp">
+     <div class="rest-1">
+     <p>Restaurant 1</p>
+     <img class="first" src="../img/rest-1.jpg"/>
+     </div>
+     <div class="rest-2">
+     <p>Restaurant 2</p>
+     <img class="middle" src="../img/rest-2.jpg"/>
+     </div>
+     <div class="rest-3">
+     <p>Restaurant 3</p>
+     <img class="last" src="../img/rest-3.jpg"/>
+     </div>
+   </div>
   </div>
 </body>
 </template>
@@ -42,28 +60,40 @@ export default {
 <style>
 .profile {
   background-image: linear-gradient(to bottom left, #FF655B, #FD297B);
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   height: 700px;
-  display: grid;
-  grid-template-columns: 1fr 3fr 1fr;
+  align-content: center;
+  padding-top: 30px;
+  /* display: grid;
+  grid-template-columns: 1fr 2fr 2fr 1fr;
   grid-template-areas:
-  ". . ."
-  ". home ."
-  ". home ."
-  ". home ."
-  ". . ."
+  "space space space space"
+  ". home home ."
+  ". home home ."
+  ". home home ."
+  ". . . ."
   ;
-  grid-gap: 10px;
-
+  grid-gap: 10px; */
 }
 
 .home {
   text-align: center;
-  background-color: white;
-  width: 700px;
+  background-color: blanchedalmond;
+
+  margin-left: 200px;
+  margin-right: 200px;
+  /* width: 700px; */
   align-content: center;
-  display: flex;
+  padding-bottom: 50px;
+  /* display: flex;
   flex-direction: column;
+  object-fit: fill; */
+  /* grid-area: home; */
 }
+
+/* .space {
+  grid-area: space;
+} */
 
 .profile-nav > .profile-navs {
   color: black;
@@ -75,15 +105,65 @@ export default {
   color: black;
 }
 
-.home > h1 {
+#my-profile {
+  text-align: center;
+}
+
+.fav {
+  text-decoration: underline;
+  text-decoration-color: lightgray;
+}
+
+#my-profile {
   font-size: 40px;
-  padding-top: 50px;
-  display: block;
-  border-bottom: 5px solid lightgray;
+  padding-top: 10px;
+  padding-bottom: 20px;
+  align-content: center;
+  text-shadow: 2px 5px 5px rgba(8, 8, 8, 0.75),
+                -5px 6px 7px rgba(8, 8, 8, 0.75);
+    color: blanchedalmond;
 }
 
 .welcome {
   font-size: 15px;
+  margin-top: -20px;
+  padding-top: 20px;
+}
+
+.middle, .first, .last {
+  width: 200px;
+  height: 100px;
+  object-fit: cover;
+}
+
+.fav-temp {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+
+.middle {
+  padding-left: -10px;
+  padding-right: -10px;
+}
+
+.rest-1, .rest-2, .rest-3 {
+  background-color: #FF5864;
+  color: white;
+  border: 3px solid black;
+}
+
+.start {
+  background-color:#ffbfb4;
+     padding-top: 5px;
+     padding-bottom: 5px;
+     padding-right: 18.5px;
+     padding-left: 18.5px;
+     border-style: outset;
+     border-width: 4px;
+     border-color:rgb(100, 38, 38);
+     margin-bottom: 2px;
+     border-radius: 15px;
 }
 
 </style>
