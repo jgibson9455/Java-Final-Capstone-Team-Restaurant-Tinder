462e7fdcf74ba46674bbc1c2680db7f3756451c0
Brianna Williams <briwill146@gmail.com>
2020-12-11 16:48:23 -0500

    matches restyling done for now

--- final-capstone/front-end/vue/src/components/Matchmaking.vue
+++ final-capstone/front-end/vue/src/components/Matchmaking.vue
@@ -9,9 +9,9 @@
         
         <img class="image" v-bind:src="randomRestaurant.imageLink"/>
         <h2 id="rest-name"> {{ randomRestaurant.restaurantName }}</h2>
-        <p id="rest-descript"> {{ randomRestaurant.Description}} The description of the restaurant will go here </p> <!--???-->
+        <p id="rest-descript"> {{ randomRestaurant.Description}} The description of the restaurant will go here. I don't want it to be this wide. How do i change it from being this wide? </p> <!--???-->
         <router-link id="details-link" v-bind:to="{name: favorites}">Click for details</router-link>
-
+    </div><!--end of random div-->
 
 
   <div class="buttons">
@@ -21,7 +21,7 @@
          v-on:click.prevent="addToDislike(randomRestaurant)">
          <img id="dislike-pic" src="../img/Dislike.png">
          </a>
-        <h5>Nope</h5>
+        <h5 class="button-label">Nope</h5>
       </div>
 
 
@@ -30,7 +30,7 @@
          v-on:click.prevent="getRandomRestaurant()">
          <img id="skip-pic" src="../img/Skip.png">
          </a>
-         <h5>Next</h5>
+         <h5 class="button-label">Next</h5>
       </div>
 
       <div id="like">
@@ -38,11 +38,11 @@
          v-on:click.prevent="addRestaurantToFavorites(randomRestaurant)">
          <img id="like-pic" src="../img/Like.png">
          </a>
-         <h5>Like</h5>
+         <h5 class="button-label">Like</h5>
       </div>
   </div> <!--end of buttons div-->
    
-    </div><!--end of random div-->
+
         <div class="match-links">
           <router-link class="your-faves" v-bind:to="{name: favorites}">View Your Favorites</router-link>
           <router-link class="all-restaurants" v-bind:to="{name: restaurants}">View All Restaurants</router-link>
@@ -115,30 +115,24 @@ export default {
 <style scoped>
 .match-main {
     display: flex;
-    align-items: center;
     flex-direction: column;
+    align-items: center;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
 .match-heading {
-    color: #FF5864;
+    color: #FF5C5C;
+    font-size: x-large;
+    margin-top: -35px;
+    margin-bottom: -20px;
     /* text-shadow: 2px 5px 5px rgba(59, 59, 57, 0.445),
                 -2px 6px 7px  rgba(59, 59, 57, 0.445);  */
     /* color: blanchedalmond; */
 }
-    
  .image {
     align-self: center;
-    height: 250px;
-    width: 250px;
-    border-style: solid;
-    border-color: black;
-    border-width: 2px;
-    border-radius: 5px; 
-}
-.main {
-    display: flex;
-    align-items: center;
-    flex-direction: column;
+    height: 275px; /*275*/
+    width: 352px; /*330*/
+    margin: -47px;
 }
 .random {
     display: flex;
@@ -149,52 +143,63 @@ export default {
     background-color: blanchedalmond;
     border-style: solid;
     border-color: black;
-    border-width: 2px;
+    border-width: 3px;
     border-radius: 5px; 
-
+    word-wrap: break-word;
+    height: 475px;
+    width: 350px;
 }
 #rest-name{
-    margin-bottom: -15px;
+    margin-bottom: -3px;
 }
 #rest-descript{
-    margin-bottom: -1px;
+    margin-bottom: 40px;
 }
 #details-link {
     color: blue;
-    font-size: medium;
+    font-size: larger;
+}
+#details-link:hover {
+    color: rgb(108, 108, 238);
 }
 .buttons {
     display: flex;
-    flex-direction: row;  
-    justify-content: space-between;
+    justify-content: space-around;
     color: gray;
-    margin-top: 5px;
+    margin-top: 10px;
+    width: 435px;
 } 
-h5 {
-    margin-top: -5px;
+.button-label {
+  text-align: center;
+  margin-top: -5px;
 }
 #dislike-pic{
-  height: 35px;
-  width: 35px;
+  height: 55px;
+  width: 55px;
 }
 #skip-pic{
-  height: 35px;
-  width: 35px;
+  height: 55px;
+  width: 55px;
 }
 #like-pic{
-  height: 35px;
-  width: 35px;
+    height: 55px;
+    width: 55px;
 }
 .match-links {
     display: flex;
     flex-direction: column;
 
 }
+.your-faves{
+    margin-top: -5px;
+    margin-bottom: 5px;
+}
 .your-faves, .all-restaurants {
-        color: blue;
+    color: blue;
+    text-align: center;
 }
 .your-faves:hover, .all-restaurants:hover {
-        color: blue;
+    color: rgb(108, 108, 238);
 }
 
 
