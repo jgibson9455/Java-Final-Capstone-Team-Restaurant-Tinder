165ef9f86f679beda73b4b3bef0def194f86e3a8
Jessica Gibson <jessicagibson4066@gmail.com>
2020-12-11 15:23:58 -0500

    styled the favorites page and the restaurants page and restaurant card

--- final-capstone/front-end/vue/src/components/FavoriteRestaurants.vue
+++ final-capstone/front-end/vue/src/components/FavoriteRestaurants.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="favorites">
-      <restaurant-card
+    <h1 class="label">My Favorites</h1>
+    <div class="rest-card">
+      <restaurant-card class="flex-container"
       v-for="restaurant in fav"
       v-bind:key="restaurant.restaurantId"
       v-bind:restaurant="restaurant"
-      />
+      ></restaurant-card></div>
+      <div class="logo"><img src="../img/logo-black.png"></div>
   </div>
+  
 </template>
 
 <script>
@@ -40,6 +44,37 @@ export default {
 
 </script>
 
-<style>
+<style scoped>
+.favorites {
+   font-size: 22px; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+   display: flex;
+   flex-direction: column;
+
+   
+}
+.label {
+  display: flex;
+  /*color: #FD297B;*/
+  color: #FF5864; 
+  justify-content: center;
+}
+.logo {
+  display: flex;
+  justify-content: center;
+  padding: 5%;
+}
+.rest-card {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  text-align: center;
+  align-items: center;
+  /*display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  
+  flex-direction: row;*/
+
+}
+
 
 </style>
