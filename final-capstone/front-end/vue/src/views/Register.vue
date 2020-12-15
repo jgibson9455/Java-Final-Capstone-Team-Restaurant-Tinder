6effc9a0d9fca8e47423299596c7f4e439fb9e34
Amanda Wolf <amandamwolf24@gmail.com>
2020-12-15 10:45:04 -0500

    register

--- final-capstone/front-end/vue/src/views/Register.vue
+++ final-capstone/front-end/vue/src/views/Register.vue
@@ -107,7 +107,7 @@ export default {
                   }
                 })
                 this.$router.push({
-                path: '/update-profile',
+                path: "/update-profile",
                 query: { registration: 'success' },
               });
             }
